4c67c5d4366ec16fefb484ff527197f86c63bfa4
Artem Petrakov <3885604@mail.ru>
2022-09-22 21:02:44 +0300

    step 4 test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,2 +1,21 @@
-import { describe, expect, it } from '@jest/globals';
+
 import genDiff from '../src/index.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// константа  __filename содержит абсолютный путь к файлу, в котором она используется
+
+const __dirname = dirname(__filename); 
+
+//константа  __dirname cодержит абсолютный путь, к каталогу.
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const expectedResult = getFixturePath('expected_file');
+
+test('Generate Differense', () => {
+  const actual = genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json');
+  expect(expectedResult).toBe(actual);
+});
