3281f13a89c8ff0bd1e81658d106cec708aa3a92
Artem Petrakov <3885604@mail.ru>
2022-09-23 13:45:54 +0300

    step 3

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,21 +1,16 @@
-
+/* eslint-disable no-undef */
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import path from 'path';
 import genDiff from '../src/index.js'
 
 const __filename = fileURLToPath(import.meta.url);
-
 // константа  __filename содержит абсолютный путь к файлу, в котором она используется
-
 const __dirname = dirname(__filename); 
-
 //константа  __dirname cодержит абсолютный путь, к каталогу.
-
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const expectedResult = getFixturePath('expected_file');
-
-
+const expectedResult = getFixturePath('expected_file.txt');
 
 test('Generate Differense', () => {
   const actual = genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json');
--- src/getDifference.js
+++ src/getDifference.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import getContent from './getContent.js'; 
+import path from 'path';
 
 // ./имядиректории - подразумевается что директория (или файла) лежит в текущей для вас директории (открытой в терминале или директори исполнения скрипта).
 // Такой формат особенно актуален при запуске исполняемых файлов -- мы как бы показываем, что запускаем не команду вообще, а именно файл из этой директории с таким именем.
@@ -9,33 +10,46 @@ export default (file1, file2) => {
   const filepath1 = getContent(file1);
   const filepath2 = getContent(file2);
 
-  const obj1 = JSON.parse(filepath1);
-  const obj2 = JSON.parse(filepath2);
+  const fileExtension = path.extname(file1);
+  // определить формат файла 
+
+  const parser = (fileExtension) => {
+    switch (fileExtension) {
+      case '.json' :
+        return JSON.parse;
+      default: 
+        console.log('Неизвестный формат')
+    }
+  };
+
+  const parserType = parser(fileExtension);
+
+  const obj1 = parserType(filepath1);
+  const obj2 = parserType(filepath2);
 
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
 
   const unionKeys = _.union(keys1, keys2).sort();
 
-  const result = unionKeys.reduce((acc, key) => {
+  const result = unionKeys.map((key) => {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+
+    if (value1 === value2) {
+      return {type: 'equal', key, value: obj1[key]}
+    }
+    if ((Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) && value1 !== value2) {
+      return {type: 'updated', key, oldValue: value1, newValue: value2}
+    }
     if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
-      acc += `\n - ${key}: ${obj1[String(key)]}`;
-    }
-    if (Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key) && obj1[key] === obj2[key]) {
-      acc += `\n   ${key}: ${obj1[key]}`;
-    }
-    if (Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key) && obj1[key] !== obj2[key]) {
-      acc += `\n - ${key}: ${obj1[key]}`;
-      acc += `\n - ${key}: ${obj2[key]}`;
+      return {type: 'removed', key, value: value1} 
     }
     if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-      acc += `\n - ${key}: ${obj2[key]}`;
+      return {type: 'added', key, value: value2} 
     }
-    return acc;
-  },'');
-
-  return `{${result}\n}`;
-  
-};
+  })
+  return result;
+  };
 
 
