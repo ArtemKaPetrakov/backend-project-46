296b718dacdba70583e8bc8dd1dc3c761931617a
Artem Petrakov <3885604@mail.ru>
2022-09-25 00:08:44 +0300

    step 4

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,6 +2,7 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'path';
+import fs from 'fs';
 import genDiff from '../src/index.js'
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,9 +11,13 @@ const __dirname = dirname(__filename);
 //константа  __dirname cодержит абсолютный путь, к каталогу.
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const expectedResult = getFixturePath('expected_file.txt');
+const resultPath = getFixturePath('expected_file.js');
+
+const result = fs.readFileSync(resultPath, 'utf8');
 
 test('Generate Differense', () => {
   const actual = genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json');
-  expect(actual).toBe(expectedResult);
+  expect(actual).toEqual(expectedResult);
 });
+
+console.log(result);
