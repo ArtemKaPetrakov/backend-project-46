1958cd3122ab857bfd9f0981ee5068d6c6a9518d
Artem Petrakov <3885604@mail.ru>
2022-09-29 11:36:24 +0300

    add displayYAML

--- src/displayResults/displayResult.js
+++ src/displayResults/displayResult.js
@@ -5,6 +5,8 @@ export default (file, fileExtension) => {
   switch (fileExtension) {
     case '.json': 
     return displayJSON(file);
+    case '.yaml' || '.yml': 
+    return displayYAML(file);
     default:
       console.log('ХЗ что делать!')
   }
