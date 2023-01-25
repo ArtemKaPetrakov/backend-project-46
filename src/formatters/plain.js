7eb4f88c953a58896bfa8da6cda36ee63702d2cf
Artem Petrakov <3885604@mail.ru>
2023-01-25 15:17:57 +0300

    fix

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -3,15 +3,14 @@ import _ from 'lodash';
 const plain = (data, path = '') => {
   // console.log(data);
   const result = data.flatMap((item) => {
-
     const isComplexValue = (value) => _.isPlainObject(value) ? '[complex value]' : typeof value === 'string' ? `'${value}'` : value;
     const { type, key, removedValue, currentValue } = item;
 
     const fullPath = [path, key].filter((item => item !== '')).join('.');
 
     switch (type) {
-      case 'equal': 
-        return ''; // без пустой строки возвращал undefined
+      case 'equal':
+        return '';// без пустой строки возвращал undefined
       case 'removed':
         return `Property '${fullPath}' was removed`;
       case 'added':
@@ -19,13 +18,15 @@ const plain = (data, path = '') => {
       case 'updated':
         return `Property '${fullPath}' was updated. From ${isComplexValue(removedValue)} to ${isComplexValue(currentValue)}`;
       case 'nested':
-        return plain(currentValue, fullPath);     
+        return plain(currentValue, fullPath);
+       default:
+        throw new Error;
     }
   }).filter(((item) => item !== '')).join('\n');
   return result;
 };
 
-export default (data) => {
-  const result = plain(data);
+export default (mainData) => {
+  const result = plain(mainData);
   return result;
 };
