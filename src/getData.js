f80473f4754e1f79659b5202760166be45a82569
Artem Petrakov <3885604@mail.ru>
2023-01-25 15:28:27 +0300

    fix

--- src/getData.js
+++ src/getData.js
@@ -4,10 +4,15 @@ import process from 'process';
 import parser from './getParser.js';
 
 const getData = (filePath) => {
-  const currentDir = process.cwd(); // текущая директория 
-  const fullPath = path.resolve(currentDir, filePath); // путь до файла 
-  const data = fs.readFileSync(fullPath, { encoding: 'utf8' }); // абсолютный путь с кодирование, иначе абракадабра
-  const dataExtension = path.extname(filePath); // расширение файла 
-  return parser(data, dataExtension) // возвращаем файлы после парсинга, зависяший от расширения
+  const currentDir = process.cwd();
+  // текущая директория
+  const fullPath = path.resolve(currentDir, filePath);
+  // путь до файла
+  const data = fs.readFileSync(fullPath, { encoding: 'utf8' });
+  // абсолютный путь с кодирование, иначе абракадабра
+  const dataExtension = path.extname(filePath);
+  // расширение файла
+  return parser(data, dataExtension);
+  // возвращаем файлы после парсинга, зависяший от расширения
 };
 export default getData;
--- src/getDifference.js
+++ src/getDifference.js
@@ -2,34 +2,34 @@ import _ from 'lodash';
 import getData from './getData.js';
 import getNewFormat from './formatters/index.js';
 
-  const generateDiff = (object1, object2) => {
-    const keys1 = Object.keys(object1);
-    const keys2 = Object.keys(object2);
-    const unionKeys = _.union(keys1, keys2).sort();
+const generateDiff = (object1, object2) => {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  const unionKeys = _.union(keys1, keys2);
+  const sortedUnionKeys = _.sortedUniq(unionKeys);
 
-    return unionKeys.map((key) => {
-
+  return sortedUnionKeys.map((key) => {
     if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
-      return {type: 'nested', key, currentValue: generateDiff(object1[key], object2[key])}
+      return { type: 'nested', key, currentValue: generateDiff(object1[key], object2[key]) };
     }
     if (object1[key] === object2[key] ) {
-      return {type: 'equal', key, currentValue: object1[key]}
+      return { type: 'equal', key, currentValue: object1[key] };
     }
     if ((Object.hasOwn(object1, key) && Object.hasOwn(object2, key)) && object1[key] !== object2[key]) {
-      return {type: 'updated', key, removedValue: object1[key], currentValue: object2[key]}
+      return { type: 'updated', key, removedValue: object1[key], currentValue: object2[key] }
     }
     if (Object.hasOwn(object1, key) && !Object.hasOwn(object2, key)) {
-      return {type: 'removed', key, currentValue: object1[key]}
+      return { type: 'removed', key, currentValue: object1[key] };
     }
     if (!Object.hasOwn(object1, key) && Object.hasOwn(object2, key)) {
-      return {type: 'added', key, currentValue: object2[key]}
+      return { type: 'added', key, currentValue: object2[key] };
     }
-  })
-  };
+  });
+};
 
-  export default (filePath1, filePath2, format) => {
-    const data1 = getData(filePath1);
-    const data2 = getData(filePath2);
-    const diff = generateDiff(data1, data2);
-    return getNewFormat(diff, format);
-  }
+export default (filePath1, filePath2, format) => {
+  const data1 = getData(filePath1);
+  const data2 = getData(filePath2);
+  const diff = generateDiff(data1, data2);
+  return getNewFormat(diff, format);
+};
