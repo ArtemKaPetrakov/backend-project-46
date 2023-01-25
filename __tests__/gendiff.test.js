b3418468250b9d36b97ecd044d8e3c54e356bb20
Artem Petrakov <3885604@mail.ru>
2023-01-25 14:33:38 +0300

    fix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,40 +6,40 @@ import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-// константа  __filename содержит абсолютный путь к файлу, в котором она используется
+//  константа  __filename содержит абсолютный путь к файлу, в котором она используется
 const __dirname = path.dirname(__filename);
-//константа  __dirname cодержит абсолютный путь, к каталогу.
-const getFixturePath = (filename) => path.join(__dirname, '..','__fixtures__', filename);
+//  константа  __dirname cодержит абсолютный путь, к каталогу.
+const getFixturePath = (filename) => path.join(__dirname,'..','__fixtures__',filename);
 
 const resultPathStylish = getFixturePath('expected_file_stylish.txt');
 const resultPathPlain = getFixturePath('expected_file_plain.txt');
 const resultPathJson = getFixturePath('expected_file_json.txt');
-// путь до файла с результатом
+//  путь до файла с результатом
 const resultStylish = fs.readFileSync(resultPathStylish, 'utf8');
 const resultPlain = fs.readFileSync(resultPathPlain, 'utf8');
 const resultJson = fs.readFileSync(resultPathJson, 'utf8');
 
 test('Generate Differense - stylish - .json', () => {
-  const actual = genDiff(getFixturePath(`file1.json`) , getFixturePath(`file2.json`), 'stylish');
+  const actual = genDiff(getFixturePath(`file1.json`),getFixturePath(`file2.json`),'stylish');
   expect(actual).toEqual(resultStylish);
 });
 test('Generate Differense - stylish - .yaml', () => {
-  const actual = genDiff(getFixturePath(`file1.yaml`) , getFixturePath(`file2.yaml`), 'stylish');
+  const actual = genDiff(getFixturePath(`file1.yaml`),getFixturePath(`file2.yaml`),'stylish');
   expect(actual).toEqual(resultStylish);
 });
 test('Generate Differense - plain - .json', () => {
-  const actual = genDiff(getFixturePath(`file1.json`) , getFixturePath(`file2.json`), 'plain');
+  const actual = genDiff(getFixturePath(`file1.json`),getFixturePath(`file2.json`),'plain');
   expect(actual).toEqual(resultPlain);
 });
 test('Generate Differense - plain - .yaml', () => {
-  const actual = genDiff(getFixturePath(`file1.yaml`) , getFixturePath(`file2.yaml`), 'plain');
+  const actual = genDiff(getFixturePath(`file1.yaml`),getFixturePath(`file2.yaml`),'plain');
   expect(actual).toEqual(resultPlain);
 });
 test('Generate Differense - JSON - .json', () => {
-  const actual = genDiff(getFixturePath(`file1.json`) , getFixturePath(`file2.json`), 'json');
+  const actual = genDiff(getFixturePath(`file1.json`),getFixturePath(`file2.json`),'json');
   expect(actual).toEqual(resultJson);
 });
 test('Generate Differense - JSON - .yaml', () => {
-  const actual = genDiff(getFixturePath(`file1.yaml`) , getFixturePath(`file2.yaml`), 'json');
+  const actual = genDiff(getFixturePath(`file1.yaml`),getFixturePath(`file2.yaml`),'json');
   expect(actual).toEqual(resultJson);
 });
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,31 +1,31 @@
-import _ from "lodash";
+import _ from 'lodash';
 
 const plain = (data, path = '') => {
   // console.log(data);
-  const result  = data.flatMap((item) => {
+  const result = data.flatMap((item) => {
 
-    const isComplexValue = (value) => _.isPlainObject(value) ? `[complex value]` : typeof value === 'string' ? `'${value}'`: value;
-      const { type, key, removedValue, currentValue } = item;
+    const isComplexValue = (value) => _.isPlainObject(value) ? `[complex value]` : typeof value === 'string' ? `'${value}'` : value;
+    const { type, key, removedValue, currentValue } = item;
 
-      const fullPath = [path, key].filter((item => item !== '')).join('.');
+    const fullPath = [path, key].filter((item => item !== '')).join('.');
 
-      switch (type) {
-        case 'equal' : 
-          return ''; // без пустой строки возвращал undefined
-        case 'removed':
-          return `Property '${fullPath}' was removed`;
-        case 'added' :
-          return `Property '${fullPath}' was added with value: ${isComplexValue(currentValue)}`; 
-        case 'updated' : 
-          return `Property '${fullPath}' was updated. From ${isComplexValue(removedValue)} to ${isComplexValue(currentValue)}`;
-        case 'nested' : 
-          return plain(currentValue, fullPath)      
-      }
-  }).filter((item => item !== '')).join('\n');
+    switch (type) {
+      case 'equal': 
+        return ''; // без пустой строки возвращал undefined
+      case 'removed':
+        return `Property '${fullPath}' was removed`;
+      case 'added':
+        return `Property '${fullPath}' was added with value: ${isComplexValue(currentValue)}`;
+      case 'updated':
+        return `Property '${fullPath}' was updated. From ${isComplexValue(removedValue)} to ${isComplexValue(currentValue)}`;
+      case 'nested':
+        return plain(currentValue, fullPath);     
+    }
+  }).filter(((item) => item !== '')).join('\n');
   return result;
-}
+};
 
 export default (data) => {
   const result = plain(data);
   return result;
-}
+};
